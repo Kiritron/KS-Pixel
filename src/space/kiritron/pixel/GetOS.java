04cfc8093e6527e4f635972ead1278babc72094f
Kiritron Stablecore <kiritron@kiritron.space>
2020-10-08 23:58:27 +0300

    Изменение лицензии.

--- src/space/kiritron/pixel/GetOS.java
+++ src/space/kiritron/pixel/GetOS.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020 Kiritron's Space
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package space.kiritron.pixel;
 
 import space.kiritron.pixel.filefunc.FileControls;
@@ -5,9 +21,7 @@ import space.kiritron.pixel.filefunc.FileControls;
 import java.io.IOException;
 
 /**
- * Класс с методами, которые позволяют получить информацию об ОС.
  * @author Киритрон Стэйблкор и MR.REX
- * @version 1.2
  */
 
 //Киритрон: Вношу изменения уже второй раз, так как немного обосрался с контролем версий и потерял
