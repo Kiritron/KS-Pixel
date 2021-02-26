8f5335574e76cd9642eb13766e9aaede980cc660
Киритрон Стэйблкор <kiritron@kiritron.space>
2021-02-26 18:23:41 +0300

    2.4. Добавлен класс VarFunc с методом isInt для проверки присутствия числа в строке и возможности спарсить это число.

---
+++ src/space/kiritron/pixel/VarFunc.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2021 Kiritron's Space
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package space.kiritron.pixel;
+
+/**
+ * @author Киритрон Стэйблкор
+ */
+
+// Класс c операциями с переменными
+public class VarFunc {
+    /**
+     * Метод для проверки строки на присутствие числа в строке, которое можно загнать под Integer.
+     * @param string Строка, которую нужно проверить.
+     * @return Возвращает TRUE, если строка содержит число, которое можно взять. Возвращает FALSE, если строка не содержит число или в строке каша, из-за которой число излвечь не получится.
+     */
+    public static boolean isInt(String string) {
+        try {
+            Integer.parseInt(string);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+}
